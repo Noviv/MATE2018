e46838e0aa5af3a48bcde8c27f07d9735892f0c4
Noviv <matthewwebb65@yahoo.com>
2018-02-17 13:52:51 -0600

    added thrust vectors to sim

--- master/test/thrust_sim/main.cpp
+++ master/test/thrust_sim/main.cpp
@@ -3,13 +3,43 @@
 
 float angle = 0;
 
+float ftl = 10;
+float fbl = 10;
+float ftr = 10;
+float fbr = 10;
+
+float rtl = 10;
+float rbl = 10;
+float rtr = 10;
+float rbr = 10;
+
+void line(float a, float b, float c, float x, float y, float z) {
+	glBegin(GL_LINES);
+	glVertex3f(a, b, c);
+	glVertex3f(a + x, b + y, c + z);
+	glEnd();
+}
+
 void drawcube(void) {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glColor3f(1.0, 1.0, 1.0);
 
 	glMatrixMode(GL_MODELVIEW);
-	glRotatef(angle, 0.0, 1.0, 0.0);
+	glRotatef(angle, 0, 1, 0);
 	glutWireCube(20);
+
+	glColor3f(1, 0, 0);
+	glLineWidth(2.5);
+	line(-10, 10, 10, ftl, -ftl, ftl);
+	line(-10, -10, 10, fbl, fbl, fbl);
+	line(10, 10, 10, -ftr, -ftr, ftr);
+	line(10, -10, 10, -fbr, fbr, fbr);
+
+	glColor3f(0, 0, 1);
+	line(-10, 10, -10, rtl, -rtl, -rtl);
+	line(-10, -10, -10, rbl, rbl, -rbl);
+	line(10, 10, -10, -rtr, -rtr, -rtr);
+	line(10, -10, -10, -rbr, rbr, -rbr);
 
 	glFlush();
 	glutSwapBuffers();
@@ -43,6 +73,7 @@ int main(int argc, char **argv) {
 	glMatrixMode(GL_MODELVIEW);
 	glClearColor(0.0, 0.0, 0.0, 0.0);
 
+	glRotatef(30, 0, 1, 0);
 	glutMainLoop();
 	return 0;
 }
