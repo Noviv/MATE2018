c9d58e575acd0cc4e079c0f4af3dcb4d1902b241
Noviv <matthewwebb65@yahoo.com>
2018-02-21 12:37:33 -0600

    migrated to GLEW & added thrust vectors

--- master/test/thrust_sim/main.cpp
+++ master/test/thrust_sim/main.cpp
@@ -1,7 +1,7 @@
-#include <GL/gl.h>
-#include <GL/glut.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
-float angle = 0;
+#include <boost/asio.hpp>
 
 float ftl = 10;
 float fbl = 10;
@@ -13,67 +13,159 @@ float rbl = 10;
 float rtr = 10;
 float rbr = 10;
 
-void line(float a, float b, float c, float x, float y, float z) {
+bool keys[GLFW_KEY_LAST];
+
+#define line(x, y, z, a, b, c) glVertex3f(x, y, z);glVertex3f(x + a, y + b, z + c);
+
+void drawCube() {
+	static float xang = 45;
+	static float yang = 45;
+	if (keys[GLFW_KEY_A]) {
+		yang--;
+	} else if (keys[GLFW_KEY_D]) {
+		yang++;
+	}
+	if (keys[GLFW_KEY_W]) {
+		xang--;
+	} else if (keys[GLFW_KEY_S]) {
+		xang++;
+	}
+	glRotatef(xang, 1, 0, 0);
+	glRotatef(yang, 0, 1, 0);
+
+	glLineWidth(1);
+
+	glColor3f(1, 0, 0);
 	glBegin(GL_LINES);
-	glVertex3f(a, b, c);
-	glVertex3f(a + x, b + y, c + z);
+	glVertex3f(0, 0, 0);
+	glVertex3f(2, 0, 0);
+	glEnd();
+
+	glColor3f(0, 1, 0);
+	glBegin(GL_LINES);
+	glVertex3f(0, 0, 0);
+	glVertex3f(0, 2, 0);
+	glEnd();
+
+	glColor3f(0, 0, 1);
+	glBegin(GL_LINES);
+	glVertex3f(0, 0, 0);
+	glVertex3f(0, 0, 2);
+	glEnd();
+
+	glColor3f(1, 1, 1);
+	glBegin(GL_LINES);
+
+	glVertex3f(1, 1, 1);
+	glVertex3f(1, 1, -1);
+
+	glVertex3f(1, 1, 1);
+	glVertex3f(1, -1, 1);
+
+	glVertex3f(1, 1, 1);
+	glVertex3f(-1, 1, 1);
+
+	glVertex3f(-1, -1, -1);
+	glVertex3f(-1, -1, 1);
+
+	glVertex3f(-1, -1, -1);
+	glVertex3f(-1, 1, -1);
+
+	glVertex3f(-1, -1, -1);
+	glVertex3f(1, -1, -1);
+	
+	glVertex3f(1, -1, -1);
+	glVertex3f(1, 1, -1);
+
+	glVertex3f(1, -1, -1);
+	glVertex3f(1, -1, 1);
+
+	glVertex3f(-1, 1, -1);
+	glVertex3f(1, 1, -1);
+
+	glVertex3f(-1, -1, 1);
+	glVertex3f(-1, 1, 1);
+
+	glVertex3f(-1, 1, 1);
+	glVertex3f(-1, 1, -1);
+
+	glVertex3f(-1, -1, 1);
+	glVertex3f(1, -1, 1);
+ 
+	glEnd();
+
+	glLineWidth(5);
+	glColor3f(0, 1, 0);
+	glBegin(GL_LINES);
+	line(1, 1, 1, -1, -1, 1);
+	line(-1, 1, 1, 1, -1, 1);
+	line(-1, -1, 1, 1, 1, 1);
+	line(1, -1, 1, -1, 1, 1);
+	glEnd();
+
+	glColor3f(1, 0, 0);
+	glBegin(GL_LINES);
+	line(1, 1, -1, -1, -1, -1);
+	line(-1, 1, -1, 1, -1, -1);
+	line(-1, -1, -1, 1, 1, -1);
+	line(1, -1, -1, -1, 1, -1);
 	glEnd();
 }
 
-void drawcube(void) {
-	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3f(1.0, 1.0, 1.0);
+void display(GLFWwindow* window) {
+	GLint width, height;
+	glfwGetWindowSize(window, &width, &height);
+	glViewport(0, 0, width, height);
 
-	glMatrixMode(GL_MODELVIEW);
-	glRotatef(angle, 0, 1, 0);
-	glutWireCube(20);
+	glClearColor(0, 0, 0, 0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	glColor3f(1, 0, 0);
-	glLineWidth(2.5);
-	line(-10, 10, 10, ftl, -ftl, ftl);
-	line(-10, -10, 10, fbl, fbl, fbl);
-	line(10, 10, 10, -ftr, -ftr, ftr);
-	line(10, -10, 10, -fbr, fbr, fbr);
+	glMatrixMode(GL_PROJECTION_MATRIX);
+	glLoadIdentity();
+	gluPerspective(60, (double) width / height, .1, 100);
 
-	glColor3f(0, 0, 1);
-	line(-10, 10, -10, rtl, -rtl, -rtl);
-	line(-10, -10, -10, rbl, rbl, -rbl);
-	line(10, 10, -10, -rtr, -rtr, -rtr);
-	line(10, -10, -10, -rbr, rbr, -rbr);
+	glMatrixMode(GL_MODELVIEW_MATRIX);
+	glTranslatef(0, 0, -5);
 
-	glFlush();
-	glutSwapBuffers();
+	drawCube();
+
+	glfwSwapBuffers(window);
+	glfwPollEvents();
 }
 
-void mouse(int btn, int state, int x, int y) {
-	if (state == GLUT_DOWN) {
-		if (btn == GLUT_LEFT_BUTTON) {
-			angle = 1;
-		} else if (btn == GLUT_RIGHT_BUTTON) {
-			angle = -1;
-		}
-	} else {
-		angle = 0;
-	}
+void keyboard(GLFWwindow* window, int key, int scancode, int action, int mods) {
+	keys[key] = action == GLFW_PRESS || action == GLFW_REPEAT;
 }
 
 int main(int argc, char **argv) {
-	glutInit(&argc, argv);
-	glutInitWindowSize(500, 500);
-	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
-	glutCreateWindow("Glut rotate");
-	glutMouseFunc(mouse);
-	glutDisplayFunc(drawcube);
-	glutIdleFunc(drawcube);
+	if (!glfwInit()) {
+		printf("no glfw\n");
+		return 1;
+	}
 
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(-30.0, 30.0, -30.0, 30.0, -30.0, 30.0);
-	glRotatef(30.0, 1.0, 0.0, 0.0);
-	glMatrixMode(GL_MODELVIEW);
-	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glfwWindowHint(GLFW_SAMPLES, 4);
 
-	glRotatef(30, 0, 1, 0);
-	glutMainLoop();
+	GLFWwindow* window = glfwCreateWindow(1024, 768, "thing", NULL, NULL);
+
+	if (!window) {
+		printf("no window\n");
+		glfwTerminate();
+		return 1;
+	}
+
+	glfwMakeContextCurrent(window);
+	glfwSetKeyCallback(window, keyboard);
+
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	glDisable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+
+	while (!glfwWindowShouldClose(window)) {
+		display(window);
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 	return 0;
 }
