11a9c66e5c4baa2ef660cda1c5a8656f1f7f1784
Noviv <mwebbmwebb@gmail.com>
2018-04-13 15:22:33 -0500

    more

--- common/include/common/xcvserialize.hpp
+++ common/include/common/xcvserialize.hpp
@@ -15,40 +15,40 @@
 #include <boost/serialization/vector.hpp>
 #include <opencv2/opencv.hpp>
 
-BOOST_SERIALIZATION_SPLIT_FREE(::cv::Mat)
+BOOST_SERIALIZATION_SPLIT_FREE(cv::Mat)
 namespace boost {
     namespace serialization {
-
-        /** Serialization support for cv::Mat */
         template <class Archive>
-        void save(Archive& ar, const ::cv::Mat& m, const unsigned int version) {
-            size_t elem_size = m.elemSize();
-            size_t elem_type = m.type();
+        void save(Archive& ar, const cv::Mat& m, const unsigned int version) {
+            size_t elemSize = m.elemSize(), elemType = m.type();
 
             ar& m.cols;
             ar& m.rows;
-            ar& elem_size;
-            ar& elem_type;
+            ar& elemSize;
+            ar& elemType; // element type.
+            size_t dataSize = m.cols * m.rows * m.elemSize();
 
-            const size_t data_size = m.cols * m.rows * elem_size;
-            ar& boost::serialization::make_array(m.ptr(), data_size);
+            for (size_t dc = 0; dc < dataSize; ++dc) {
+                ar& m.data[dc];
+            }
         }
 
-        /** Serialization support for cv::Mat */
         template <class Archive>
-        void load(Archive& ar, ::cv::Mat& m, const unsigned int version) {
+        void load(Archive& ar, cv::Mat& m, const unsigned int version) {
             int cols, rows;
-            size_t elem_size, elem_type;
+            size_t elemSize, elemType;
 
             ar& cols;
             ar& rows;
-            ar& elem_size;
-            ar& elem_type;
+            ar& elemSize;
+            ar& elemType;
 
-            m.create(rows, cols, elem_type);
+            m.create(rows, cols, elemType);
+            size_t dataSize = m.cols * m.rows * elemSize;
 
-            size_t data_size = m.cols * m.rows * elem_size;
-            ar& boost::serialization::make_array(m.ptr(), data_size);
+            for (size_t dc = 0; dc < dataSize; ++dc) {
+                ar& m.data[dc];
+            }
         }
 
     } // namespace serialization
