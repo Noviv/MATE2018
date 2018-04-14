bd749fc64d298c5ce0de63ec37c47c843416f9ca
Noviv <mwebbmwebb@gmail.com>
2018-04-14 14:14:33 -0500

    added wip for non-boost net camera

---
+++ wip/recv.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#include "opencv2/opencv.hpp"
+
+int main() {
+	int sokt;
+	const char* ip = "10.42.0.58";
+	int port = 4123;
+
+	struct sockaddr_in serverAddr;
+	socklen_t addrLen = sizeof(struct sockaddr_in);
+
+	if ((sokt = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+		std::cerr << "socket() failed" << std::endl;
+		return 1;
+	}
+
+	serverAddr.sin_family = PF_INET;
+	serverAddr.sin_addr.s_addr = inet_addr(ip);
+	serverAddr.sin_port = htons(port);
+
+	if (connect(sokt, (sockaddr*) &serverAddr, addrLen) < 0) {
+		std::cerr << "connect() failed" << std::endl;
+		return 1;
+	}
+
+	cv::Mat img(480, 640, CV_8UC1);
+	int sz = img.total() * img.elemSize();
+	uchar* iptr = img.data;
+	int bytes = 0;
+
+	if (!img.isContinuous()) {
+		img = img.clone();
+	}
+
+	std::cout << "sz : " << sz << std::endl;
+
+	cv::namedWindow("recv", 1);
+
+	while (1) {
+		if ((bytes = recv(sokt, iptr, sz, MSG_WAITALL)) == -1) {
+			std::cerr << "recv failed" << std::endl;
+			return 1;
+		}
+
+		cv::imshow("recv", img);
+
+		int wk = cv::waitKey(10);
+		if (wk >= 0 && wk != 255) {
+			break;
+		}
+	}
+
+	close(sokt);
+}
---
+++ wip/serv.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "opencv2/opencv.hpp"
+
+cv::VideoCapture cap(0);
+
+void display(int* ptr) {
+	int sokt = *ptr;
+
+	cv::Mat img(480, 640, CV_8UC1);
+	cv::Mat imgGray;
+	if (!img.isContinuous()) {
+		img = img.clone();
+	}
+
+	int sz = img.total() * img.elemSize();
+	int bytes = 0;
+
+	while (1) {
+		cap >> img;
+
+		cvtColor(img, imgGray, CV_BGR2GRAY);
+
+		if ((bytes = send(sokt, imgGray.data, sz, 0)) < 0) {
+			std::cerr << "send() failed" << std::endl;
+			break;
+		}
+	}
+}
+
+int main() {
+	if (!cap.isOpened()) {
+		std::cerr << "cap not opened" << std::endl;
+		return 1;
+	}
+
+	int lSokt;
+	int rSokt;
+	int port = 4123;
+
+	struct sockaddr_in lAddr;
+	struct sockaddr_in rAddr;
+
+	int addrLen = sizeof(struct sockaddr_in);
+
+	lSokt = socket(AF_INET, SOCK_STREAM, 0);
+	if (lSokt == -1) {
+		std::cerr << "lSokt == -1" << std::endl;
+	}
+	lAddr.sin_addr.s_addr = INADDR_ANY;
+	lAddr.sin_port = htons(port);
+
+	if (bind(lSokt, (struct sockaddr*) &lAddr, sizeof(lAddr)) < 0) {
+		std::cerr << "bind() failed" << std::endl;
+		return 1;
+	}
+
+	listen(lSokt, 3);
+
+	std::cout << "waiting for a connection" << std::endl;
+
+	rSokt = accept(lSokt, (struct sockaddr*) &rAddr, (socklen_t*) &addrLen);
+
+	if (rSokt < 0) {
+		std::cerr << "rSokt < 0" << std::endl;
+		return 1;
+	}
+
+	std::cout << "connection accepted" << std::endl;
+	display(&rSokt);
+}
